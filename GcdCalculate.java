a19be23ccc2b2eea12072a74ad844ffec06a5656
nilbindas1990 <nilbindas@gmail.com>
2018-03-21 13:36:52 +0530

    GCD funtion Using recusion

--- GcdCalculate.java
+++ GcdCalculate.java
@@ -11,18 +11,38 @@ public class GcdCalculate {
         x = s.nextInt();
         y = s.nextInt();
         boolean a = (x >= 1 && x<= 100000) && (y >= 1 && y <= 100000);
-        int[] factor1;
-        int[] factor2;
-        int result;
+        int result = 0;
         if (a){
-            result = gcdCalculate(x,y);
+            //Calling function which will return the result as an int.
+            //Comparing the numbers.
+            if (x == y){
+                result = x;
+            }
+            else if (x > y){
+                result = gcdCalculate(x , y);
+            }
+            else {
+                result = gcdCalculate(y , x);
+            }
+
             
         }
+        System.out.println(result);
 
     }
 
+    /*Here you will use a recursive function
+    which will call it self unless the "remainder" is 0
+     */
     private static int gcdCalculate(int x, int y) {
-        
 
+        int temp;
+        if (x % y != 0){
+                temp = x % y;
+                x = y;
+                y = temp;
+                gcdCalculate(x , y);
+        }
+        return y;
     }
 }
