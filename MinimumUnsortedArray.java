6ccd37ce6685233d3a0c3e42c6dbb54c5d8a1adc
nilbindas1990 <nilbindas@gmail.com>
2018-03-21 22:30:12 +0530

    Minimum unsotred array vrsion-1

---
+++ MinimumUnsortedArray.java
@@ -0,0 +1,26 @@
+package com.example.JavaExamples;
+
+import java.util.Scanner;
+
+public class MinimumUnsortedArray {
+
+    public static void main(String []args){
+
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        if (n >= 1 && n <= 100000){
+            int[] fullArray = new int[n];
+            for (int i = 0; i < n; i++){
+
+                fullArray[i] = s.nextInt();
+            }
+            for (int j = 0; j < fullArray.length; j++){
+
+
+            }
+        }
+
+
+
+    }
+}
