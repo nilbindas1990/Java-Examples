7222fe5fc6b65f451f3113aa92bc8a442e680519
nilbindas1990 <nilbindas@gmail.com>
2018-03-23 19:17:13 +0530

    /*This is a simple program to find the average of total sum of even and odd numbers*/

---
+++ OddEvenAverage.java
@@ -0,0 +1,46 @@
+/*This is a simple program to find the average of total sum of even and odd numbers*/
+package com.example.JavaExamples;
+
+import java.util.Scanner;
+
+public class OddEvenAverage {
+
+    public static void main(String []args){
+
+        Scanner s = new Scanner(System.in);
+        int sizeOf = s.nextInt();
+
+        int[] numArray = new int[sizeOf];
+        int oddSum = 0 , evenSum = 0;
+        int oddCount = 0 , evenCount = 0;
+        if (sizeOf > 1 && sizeOf < 100000) {
+
+            for (int i = 0; i < sizeOf; i++) {
+                int toNum = s.nextInt();
+                if (toNum > 1 && toNum < 100000) {
+                    numArray[i] = toNum;
+                }
+            }
+        }
+
+        for (int i = 0; i < numArray.length; i++) {
+            int temp = (numArray[i] % 2);
+
+            if (temp == 0) {
+                evenSum += numArray[i];
+                evenCount += 1;
+            } else {
+                oddSum += numArray[i];
+                oddCount += 1;
+            }
+        }
+
+        int evenAvg = evenSum / evenCount;
+        int oddAvg = oddSum / oddCount;
+        int sum = evenAvg + oddAvg;
+
+        System.out.print(sum);
+
+
+    }
+}
